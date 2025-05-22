d73aa25337d755c859a2f7983630b2ec9f13df26
Lila M. <lila.msfrazao@gmail.com>
2025-05-21 21:57:50 -0300

    Alterei exercicio 01

--- pratica02/01.py
+++ pratica02/01.py
@@ -15,4 +15,5 @@ taxa_euro = 6.15
 print(f"O valor de R$ {valor_reais} convertido para dólar é $ {converte_moeda(valor_reais, taxa_dolar):.2f}")
 print(f"O valor de R$ {valor_reais} convertido para euro é € {converte_moeda(valor_reais, taxa_euro):.2f}")
 
+print("Oi")
 # Lila: Meu Deus, eu tô me sentindo pobre. 100 reais é quase trocado pra eles
